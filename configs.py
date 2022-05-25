50ee8c379ed9c4f9dd7f99e02ae2c574c9a04454
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-05-25 14:36:14 +0530

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,26 @@
+# (c) @AbirHasan2005
+
+import os
+import logging
+
+logging.basicConfig(
+    format='%(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('log.txt'),
+              logging.StreamHandler()],
+    level=logging.INFO
+)
+
+
+class Config(object):
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
+    LOGGER = logging
+    OWNER_ID = int(os.environ.get("OWNER_ID", 5339194287))
+    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
+    PRO_USERS.append(OWNER_ID)
+    MONGODB_URI = os.environ.get("MONGODB_URI", "")
+    FORCE_SUB = os.environ.get("FORCE_SUB", "beta_bot_updates")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
